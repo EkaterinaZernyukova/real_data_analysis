b206eea1ef83b13604b287ccd19e240ca352a921
kate <wondalisa@gmail.com>
2021-01-13 18:54:53 +0300

    Первые 5 задач iledebeaute

--- iledebeaute_interview_task/iledebeaute.sql
+++ iledebeaute_interview_task/iledebeaute.sql
@@ -1,25 +1,27 @@
-
-
-ALTER TABLE orders RENAME TO A1;
-
-ALTER TABLE A1 RENAME COLUMN TEXT TO VERSIONS;
-ALTER TABLE A1 RENAME COLUMN STATUS TO TEXT;
-
-ALTER TABLE a1 ALTER COLUMN text TYPE varchar(255);
-
-ALTER TABLE departments RENAME TO A2;
-ALTER TABLE A2 RENAME COLUMN depname TO text;
-ALTER TABLE a2 ALTER COLUMN text TYPE varchar(255);
-INSERT INTO a VALUES (1, '01.07.2018'), (234, '04.10.2018'), (234, '02.07.2018'), (3, '02.07.2018'), (1, '01.07.2018');
-UPDATE A1 SET TEXT = 'ANY' WHERE ID = 9;
-
---DELETE FROM A1 WHERE TEXT = 'ANY';
-
 --1.Составить запрос, выдающий все строки таблицы A1, id которых есть в таблице A2
-SELECT * FROM a1 WHERE id IN (SELECT id FROM a2);
+SELECT
+	*
+FROM
+	a1
+WHERE
+	id IN (
+	SELECT
+		id
+	FROM
+		a2);
 
 --2.Составить запрос, выдающий все строки таблицы A1, id которых нет в таблице A2
-SELECT * FROM a1 WHERE id NOT IN (SELECT id FROM a2);
+
+SELECT
+	*
+FROM
+	a1
+WHERE
+	id NOT IN (
+	SELECT
+		id
+	FROM
+		a2);
 
 --3. Составить запрос, выдающий A1.ID, A1.TEXT, A2.TEXT для A1.ID = A2.ID. 
 --Если в таблице A2 нет соответствующего ID вместо A2.TEXT вывести слово “НЕТ ”
@@ -36,7 +38,6 @@ FROM
 LEFT JOIN a2 ON
 	A1.id = A2.id;
 
-
 --4. Изменить значение TEXT в таблице A1 на значение TEXT из таблицы A2 для A1.ID =  A2.ID
 
 UPDATE
@@ -48,12 +49,19 @@ FROM
 WHERE
 	A1.id = A2.id;
 
+--5. Найти в таблице A1 ID повторяющиеся более 2-х раз ID и подсчитать их количество
 
+SELECT
+    id, COUNT(*)
+FROM
+    A1
+GROUP BY
+    id
+HAVING 
+    COUNT(*) > 2;
 
+--6. Вставить в таблицу A1 только те ID и TEXT из таблицы A2, которых нет в A1
 
-5.
-
-6. 
 INSERT INTO a1 (TEXT) SELECT TEXT FROM a2 WHERE a1.id=a2.id;
 
 --7.Удалить из таблицы A1 те строки, ID которых есть в A2
--- iledebeaute_interview_task/initialization_a1 table.sql
+++ iledebeaute_interview_task/initialization_a1 table.sql
@@ -1,21 +1,17 @@
 CREATE TABLE public.a1 (
-	id int4 NOT NULL DEFAULT nextval('orders_id_seq'::regclass),
-	"name" int4 NULL,
-	mix varchar(20) NULL,
-	versions varchar(255) NOT NULL DEFAULT 'NOT'::character varying,
-	indexs _varchar NULL,
+	id int4 NOT NULL,
 	"text" varchar(255) NULL,
 	CONSTRAINT orders_pkey PRIMARY KEY (id),
 	CONSTRAINT orders_users_check CHECK ((name > 0))
 );
 
 
-ALTER TABLE public.a1 ADD CONSTRAINT orders_users_fkey FOREIGN KEY (name) REFERENCES present(user_id);
 
 
 INSERT INTO public.a1 ("name",mix,versions,indexs,"text") VALUES
-	 (2,'6','NOT',NULL,'min'),
-	 (3,'6','NOT',NULL,'min'),
-	 (1,'6','NOT',NULL,'min'),
-	 (NULL,'4','NOT',NULL,'max'),
-	 (NULL,NULL,'ANY','{sql,postgres,database,plsql}',NULL);
+	 (2,'min'),
+	 (6, NULL),
+	 (9,'min'),
+	 (7,'min'),
+	 (9,'max'),
+	 (8,,NULL);
