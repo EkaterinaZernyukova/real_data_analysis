551c8ca69b9bba3bf679d75a5820c2b7d6a34294
kate <wondalisa@gmail.com>
2021-01-24 17:08:18 +0300

    sql АВ-тест

--- wargaming_task/wargaming_task_2.sql
+++ wargaming_task/wargaming_task_2.sql
@@ -41,3 +41,54 @@ ORDER BY
 
 --Агрегирование выручки по группам Type_of_group
 
+--средняя выручка, общая выручка и количество операций в группах (wargaming_agg)
+CREATE TEMPORARY TABLE wargaming_agg("Type_of_group" text NULL,
+	"summ" float8 NULL,
+	"avg_revenue" float8 NULL,
+	"count_transaction" int8 NULL
+);
+
+--INSERT INTO wargaming_agg
+ SELECT
+	DISTINCT "Type_of_group",
+	sum (wargaming_all."Revenue") OVER (PARTITION BY wargaming_all."Type_of_group") AS summ,
+	avg(wargaming_all."Revenue") OVER (PARTITION BY wargaming_all."Type_of_group") as avg_revenue,
+	count(wargaming_all."Revenue") OVER (PARTITION BY wargaming_all."Type_of_group") AS count_transaction
+FROM
+	wargaming_all;
+
+
+--количество уникальных id в группах (wargaming_uniq_id_pay), пользователей, которые платили
+CREATE TEMPORARY TABLE wargaming_uniq_id_pay("Type_of_group" text NULL,
+	"count_id_pay" int8 NULL
+);
+
+--INSERT INTO wargaming_uniq_id_pay
+SELECT "Type_of_group", count (DISTINCT "SPA_ID") as count_id_pay FROM wargaming_all group by "Type_of_group";
+
+--количество уникальных id в группах (wargaming_uniq_id_group)
+
+CREATE TEMPORARY TABLE wargaming_uniq_id_group("Type_of_group" text NULL,
+	"count_id_max" int8 NULL
+);
+
+--INSERT INTO wargaming_uniq_id_group
+SELECT "Type_of_group", count (DISTINCT "SPA_ID") as count_id_max FROM wargaming group by "Type_of_group";
+
+-- сводная таблица агрегированных значений по группам
+CREATE TEMPORARY TABLE wargaming_total("Type_of_group" text NULL,
+	"summ" float8 NULL,
+	"avg_revenue" float8 NULL,
+	"count_transaction" int8 NULL,
+	"count_id_pay" int8 NULL,
+	"count_id_max" int8 NULL
+);
+
+
+--INSERT INTO wargaming_total
+SELECT * from wargaming_agg LEFT JOIN wargaming_uniq_id_pay USING ("Type_of_group")
+LEFT JOIN wargaming_uniq_id_group USING ("Type_of_group")
+
+--Итог
+Select*from wargaming_total
+
