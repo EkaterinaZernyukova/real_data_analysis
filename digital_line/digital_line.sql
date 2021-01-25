8eb3019d6824c6dadaa384c1417ec75c26d8f8e4
kate <wondalisa@gmail.com>
2021-01-26 00:13:52 +0300

    SQL агрегирование

--- digital_line/digital_line.sql
+++ digital_line/digital_line.sql
@@ -1,19 +1,15 @@
-SQL Есть 2 таблицы с информацией о клиентах (customer_info.xlsx) и транзакциях за период с 01.06.2015 по 01.06.2016 (Transactions_info.xlsx). 
-	Необходимо выполнить следующие задания: 
-Вывести список клиентов с непрерывной историей за год,
+SQL Есть 2 таблицы с информацией о клиентах (customer_info.xlsx) и транзакциях за период с 01.06.2015 по 01.06.2016 (Transactions_info.xlsx). Необходимо выполнить следующие задания: Вывести список клиентов с непрерывной историей за год,
 средний чек за период,
 средняя сумма покупок за месяц,
-количество всех операций по клиенту за период 
-	Вывести помесячную информацию: 
-средняя сумма чека в месяц,
+количество всех операций по клиенту за период Вывести помесячную информацию: средняя сумма чека в месяц,
 среднее количество операций в месяц,
-среднее количество клиентов, которые совершали операции;
+среднее количество клиентов,
+которые совершали операции;
+
 Долю от общего количества операций за год и долю в месяц от общей суммы операций;
 
-Вывести % соотношение M / F / NA в каждом месяце с их долей затрат 
-Вывести возрастные группы клиентов с шагом 10 лет и отдельно клиентов,
+Вывести % соотношение M / F / NA в каждом месяце с их долей затрат Вывести возрастные группы клиентов с шагом 10 лет и отдельно клиентов,
 у которых нет данной информации с параметрами
-
 --Удаление лишних символов из transactions_info."ID_client"
  UPDATE
 	transactions_info
@@ -29,14 +25,12 @@ UPDATE
 	transactions_info
 SET
 	"ID_client" = REPLACE("ID_client", '_', '');
-
 --  Изменение типа данных
  ALTER TABLE transactions_info ALTER COLUMN "ID_client" TYPE integer
 	USING ("ID_client"::integer);
 
 ALTER TABLE digital_all ALTER COLUMN "date_new" TYPE timestamp
 	USING ("date_new"::timestamp);
-
 --Добавление нового столбца
  ALTER TABLE transactions_info ADD COLUMN "Id_client" integer;
 
@@ -46,7 +40,6 @@ SET
 	"Id_client" = "ID_client";
 
 ALTER TABLE transactions_info DROP COLUMN "ID_client";
-
 --сводная таблица данных
  CREATE TEMPORARY TABLE digital_all("Id_client" int8 NULL, "Total_amount" int8 NULL, "Gender" TEXT NULL, "Age" float8 NULL, "Count_city" int8 NULL, "Response_communcation" int8 NULL, "Communication_3month" int8 NULL, "Tenure" int8 NULL, "date_new" TEXT NULL, "Id_check" int8 NULL, "Count_products" float8 NULL, "Sum_payment" float8 NULL );
 --INSERT INTO digital_all
@@ -86,7 +79,6 @@ GROUP BY
 	1
 ORDER BY
 	1;
-
 --4. количество всех операций по клиенту за период
  SELECT
 	"Id_client",
@@ -107,7 +99,6 @@ GROUP BY
 	1
 ORDER BY
 	1;
-
 --помесячно по клиентам средняя сумма чека в месяц
  SELECT
 	"Id_client",
@@ -133,8 +124,11 @@ FROM
 GROUP BY
 	1;
 
-SELECT *, avg("monthly_check") over( ) as average_monthly_check FROM digital_chesk
-
+SELECT
+	*,
+	avg("monthly_check") OVER( ) AS average_monthly_check
+FROM
+	digital_chesk
 --помесячно по клиентам среднее количество операций в месяц
  SELECT
 	"Id_client",
@@ -146,14 +140,13 @@ GROUP BY
 ORDER BY
 	1;
 
-
 --7. среднее количество клиентов, которые совершали операции; 
+
 --(столбец со средним можно добавить через временную таблицу и avg в оконной функции как в задании 6)
  SELECT
 	count (DISTINCT "Id_client")/ 13 AS average_count_clients_per_month
 FROM
 	digital_all;
-
 --количество клиентов, которые совершали операции ежемесячно 
  SELECT
 	to_char("date_new", 'YY-Mon') AS year_month,
@@ -183,11 +176,34 @@ ORDER BY
 SELECT
 	"date_new",
 	ROUND((100.0 * count_checks_per_month / sum(count_checks_per_month) OVER ())::NUMERIC, 2) AS percent_check,
-	ROUND((100.0 * sum_per_month / sum(sum_per_month) OVER ())::NUMERIC, 2) AS percent_sum
+	ROUND(( count_checks_per_month / sum(count_checks_per_month) OVER ())::NUMERIC, 2) AS share_check,
+	ROUND((100.0 * sum_per_month / sum(sum_per_month) OVER ())::NUMERIC, 2) AS percent_sum,
+	ROUND((sum_per_month / sum(sum_per_month) OVER ())::NUMERIC, 2) AS share_sum
 FROM
 	digital_share;
-	
+
 --9. Вывести % соотношение M / F / NA в каждом месяце с их долей затрат 
+--сводная таблица данных
+ CREATE TEMPORARY TABLE digital_gender("date_new" TEXT NULL, "Gender" TEXT NULL, "count_gender" int8 NULL, "cost_gender" float8 NULL);
+--INSERT INTO digital_gender
+ SELECT
+	to_char("date_new", 'YY-Mon') AS year_month,
+	"Gender",
+	count(DISTINCT "Id_client") AS count_gender,
+	ROUND(sum("Sum_payment")::NUMERIC, 2) AS cost_gender
+FROM
+	digital_all
+GROUP BY
+	1,
+	2;
 
+SELECT
+	*,
+	ROUND((100.0 * count_gender / sum(count_gender) OVER (PARTITION BY "date_new"))::NUMERIC, 2) AS percent_gender,
+	ROUND((cost_gender / sum(cost_gender) OVER (PARTITION BY "date_new"))::NUMERIC, 2) AS share_cost
+FROM
+	digital_gender
+ORDER BY
+	1
 
-
+--	10. Вывести возрастные группы клиентов с шагом 10 лет и отдельно клиентов, у которых нет данной информации с параметрами
